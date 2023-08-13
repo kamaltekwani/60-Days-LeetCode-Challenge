118de1d5d28a2bcf9fc8ecb158830e6c26cb9cd7
kamaltekwani <44347083+kamaltekwani@users.noreply.github.com>
2023-08-13 15:14:08 -0400

    Initial Commit

---
+++ Day 34/Q39_Combination_Sum.java
@@ -0,0 +1,40 @@
+/*
+ * Problem Link : https://leetcode.com/problems/combination-sum/
+ * Solution Link :  https://www.youtube.com/watch?v=obBSF4AG3qU
+ *
+ * Time Complexity : 
+ * Space Compexity : O(1)
+ *
+ */
+
+class Q39_Combination_Sum
+{
+    public void findCombinationSum(int candidates[], int start, int target, List<Integer> list, List<List<Integer>> result)
+    {
+        if(target == 0)
+        {
+            result.add(new ArrayList<Integer>(list));
+            return;
+        }
+
+        if(target<0)
+            return;
+
+        for(int index=start ; index<candidates.length ; index++)
+        {
+            list.add(candidates[index]);
+            findCombinationSum(candidates, index, target-candidates[index], list, result);
+            list.remove(list.size()-1);
+        }
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) 
+    {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> list = new ArrayList<Integer>();
+        findCombinationSum(candidates, 0, target, list, result);
+        return result;
+    }
+
+
+}
